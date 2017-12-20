62253c9b7d1bf75c03d0d7e83936bd1d6ab48057
Bearvalm <34692381+Bearvalm@users.noreply.github.com>
2017-12-20 02:51:07 +0200

    Update TestMatrixUtil.java

--- src/test/java/TestMatrixUtil.java
+++ src/test/java/TestMatrixUtil.java
@@ -79,7 +79,7 @@ public class TestMatrixUtil {
      */
     @Test(expected = InvalidMatrixException.class)
     public void testArrayIsNotMatrix() {
-        awbcnew MatrixUtil().sort(new int[][]{{2, 4}, {}, {1}}, 3);
+        new MatrixUtil().sort(new int[][]{{2, 4}, {}, {1}}, 3);
     }
 
 
